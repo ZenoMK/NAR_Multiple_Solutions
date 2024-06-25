d149b5e40fc051dc3835d3bc849ee98624790616
ZenoMK <zeno.kujawa@gmail.com>
2024-06-25 23:06:00 +0100

    updated plotting code: now works with comparing BF too!

--- clrs/_src/test_rerunning_number/analyze_data.py
+++ clrs/_src/test_rerunning_number/analyze_data.py
@@ -9,17 +9,18 @@ with plt.style.context(spstyle.get_style('nature-reviews')):
 min_size = 5
 max_size = 64
 means20_50, std20_50 = generate_data(graphs_per_size=100, num_solutions=[20,50])
-
+print("here1")
 plt.plot([i for i in range(min_size, max_size + 1)], means20_50, label = "20 vs 50 re-runs")
 plt.fill_between(x = [i for i in range(min_size, max_size + 1)], y1 = means20_50 + std20_50, y2 = means20_50 - std20_50, alpha = 0.3)
 
 means50_100, std50_100 = generate_data(graphs_per_size=100, num_solutions=[50,100])
 plt.plot([i for i in range(min_size, max_size + 1)], means50_100, label = "50 vs 100 re-runs")
 plt.fill_between(x = [i for i in range(min_size, max_size + 1)], y1 = means50_100 + std50_100, y2 = means50_100 - std50_100, alpha = 0.3)
-
+print("here2")
 means20_100, std20_100 = generate_data(graphs_per_size=100, num_solutions=[20,100])
 plt.plot([i for i in range(min_size, max_size + 1)], means20_100, label = "20 vs 100 re-runs")
 plt.fill_between(x = [i for i in range(min_size, max_size + 1)], y1 = means20_100 + std20_100, y2 = means20_100 - std20_100, alpha = 0.3)
+print("here3")
 plt.legend(loc = "lower right")
 plt.xlabel("Graph sizes")
 plt.ylabel("KL-Divergence")
@@ -34,12 +35,13 @@ plt.tight_layout()
 means20_50, std20_50 = generate_data(graphs_per_size=100, num_solutions=[20,50], algorithm='bf')
 plt.plot([i for i in range(min_size, max_size + 1)], means20_50, label = "20 vs 50 re-runs")
 plt.fill_between(x = [i for i in range(min_size, max_size + 1)], y1 = means20_50 + std20_50, y2 = means20_50 - std20_50, alpha = 0.3)
-
+print("here4")
 means50_100, std50_100 = generate_data(graphs_per_size=100, num_solutions=[50,100], algorithm = 'bf')
 plt.plot([i for i in range(min_size, max_size + 1)], means50_100, label = "50 vs 100 re-runs")
 plt.fill_between(x = [i for i in range(min_size, max_size + 1)], y1 = means50_100 + std50_100, y2 = means50_100 - std50_100, alpha = 0.3)
-
+print("here5")
 means20_100, std20_100 = generate_data(graphs_per_size=100, num_solutions=[20,100], algorithm = 'bf')
+print("here6")
 plt.plot([i for i in range(min_size, max_size + 1)], means20_100, label = "20 vs 100 re-runs")
 plt.fill_between(x = [i for i in range(min_size, max_size + 1)], y1 = means20_100 + std20_100, y2 = means20_100 - std20_100, alpha = 0.3)
 plt.legend(loc = "lower right")
