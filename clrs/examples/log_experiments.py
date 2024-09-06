9b17e418439558c02606db65b328e25fe20d4285
ZenoMK <zeno.kujawa@gmail.com>
2024-09-05 21:22:44 -0400

    make a grand old flag

--- clrs/examples/log_experiments.py
+++ clrs/examples/log_experiments.py
@@ -65,10 +65,10 @@ def BF_collect_and_eval(sampler, predict_fn, sample_count, rng_key, extras, file
         print('log_exp.py, vd_flag working')
         print('log_exp.py, BF_collect_and_eval, only testing on 1 of the graphs')
         #truncate to just one graph
-        As = As[:1]
-        source_nodes = source_nodes[:1]
-        outputs[0].data = outputs[0].data[:1]
-        preds['pi'].data = preds['pi'].data[:1]
+        As = As[:10]
+        source_nodes = source_nodes[:10]
+        outputs[0].data = outputs[0].data[:10]
+        preds['pi'].data = preds['pi'].data[:10]
         #truncate
         dataframes,_,_ = validate_distributions(As=As, Ss=source_nodes, outsOrPreds=[preds], numSolsExtracting=NSE, flag='BF')    # note wrapping preds in list for extract_probmatrices to work
         plot_n_unique_by_n_extracted(dataframes, len(As[0]))
@@ -244,6 +244,9 @@ def DFS_collect_and_eval(sampler, predict_fn, sample_count, rng_key, extras, fil
 
     if vd_flag:
         print('log_exp.py, vd_flag working')
+        As = As[:10]
+        outputs[0].data = outputs[0].data[:10]
+        preds['pi'].data = preds['pi'].data[:10]
         dataframes, As, pMs = validate_distributions(As=As, Ss=[0]*len(As), outsOrPreds=preds,
                                             numSolsExtracting=NSE, flag='DFS')  # note not wrapping preds in list for extract_probmatrices to work
         #breakpoint()
