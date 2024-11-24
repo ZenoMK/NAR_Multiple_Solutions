abcc5a22e20b115b4e10f2d4959c79f65718d3f1
Zeno Kujawa <30478937+ZenoMK@users.noreply.github.com>
2024-11-23 22:40:16 -0500

    batch size reset, fix vd = 10 bug for BF

--- clrs/examples/log_experiments.py
+++ clrs/examples/log_experiments.py
@@ -65,14 +65,16 @@ def BF_collect_and_eval(sampler, predict_fn, sample_count, rng_key, extras, file
         print('log_exp.py, vd_flag working')
         print('log_exp.py, BF_collect_and_eval, only testing on 10 of the graphs')
         #truncate to just 10 graph
-        As = As[:10]
-        source_nodes = source_nodes[:10]
-        outputs[0].data = outputs[0].data[:10]
-        preds['pi'].data = preds['pi'].data[:10]
+        As_vd = As[:10]
+        source_nodes_vd = source_nodes[:10]
+        outputs_vd = outputs
+        preds_vd = preds
+        outputs_vd[0].data = outputs[0].data[:10]
+        preds_vd['pi'].data = preds['pi'].data[:10]
         #truncate
-        dataframes,_,_ = validate_distributions(As=As, Ss=source_nodes, outsOrPreds=[preds], numSolsExtracting=NSE, flag='BF')    # note wrapping preds in list for extract_probmatrices to work
+        dataframes,_,_ = validate_distributions(As=As_vd, Ss=source_nodes_vd, outsOrPreds=[preds_vd], numSolsExtracting=NSE, flag='BF')    # note wrapping preds in list for extract_probmatrices to work
         plot_n_unique_by_n_extracted(dataframes, len(As[0]))
-        df, _, _ = validate_distributions(As=As, Ss=source_nodes, outsOrPreds=[preds], numSolsExtracting=NSE,
+        df, _, _ = validate_distributions(As=As_vd, Ss=source_nodes_vd, outsOrPreds=[preds_vd], numSolsExtracting=NSE,
                                             flag="dummy", edge_reuse_BF=True)
 
         #plot_edge_reuse_matrix_list_median(df, len(As[0]))
